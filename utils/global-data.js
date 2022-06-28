f2de20cdbbee3e37cb06b75700dfa02597b6cb90
yvonne <zhangyf-o@glodon.com>
2022-06-29 01:29:13 +0800

    fix

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Yvonne,s learning diary';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'InterView Record';
+    : 'Paper Reading';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : '.';
